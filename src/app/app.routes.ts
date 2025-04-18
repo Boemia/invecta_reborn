98aa3761fe2d7342abcfe1350406bf2fee4d740d
Gabriel Resende <gabrielresende091002@gmail.com>
2025-04-18 10:12:16 -0300

    the start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { DefaultMainLayoutComponent } from './components/default-main-layout/default-main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "main",
+        component: DefaultMainLayoutComponent
+    }
+];
+6
---
+++ src/app/components/default-main-layout/default-main-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-main-layout',
+  imports: [],
+  templateUrl: './default-main-layout.component.html',
+  styleUrl: './default-main-layout.component.scss'
+})
+export class DefaultMainLayoutComponent {
+
+}
